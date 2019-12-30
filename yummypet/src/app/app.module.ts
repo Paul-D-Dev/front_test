0d5ee7e7af4ff91c608ee5d294926f699889f0c7
Paul DAO <dao.van.paul@gmail.com>
2019-12-30 17:43:29 +0100

    create architecture

--- yummypet/src/app/app.module.ts
+++ yummypet/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPetComponent } from './components/add-pet/add-pet.component';
+import { ListPetsComponent } from './components/list-pets/list-pets.component';
+import { PetsComponent } from './pages/pets/pets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPetComponent,
+    ListPetsComponent,
+    PetsComponent
   ],
   imports: [
     BrowserModule,
---
+++ yummypet/src/app/components/list-pets/list-pets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pets',
+  templateUrl: './list-pets.component.html',
+  styleUrls: ['./list-pets.component.scss']
+})
+export class ListPetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
